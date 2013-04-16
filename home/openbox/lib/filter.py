a806aa8f7ac068d21706c5fad6c0ba7026f2d31c
Oleg Pudeyev <oleg@bsdpower.com>
2013-04-16 16:51:38 -0400

    Generic openbox menu

--- home/openbox/lib/filter.py
+++ home/openbox/lib/filter.py
@@ -2,6 +2,5 @@ import pyratemp
 import req, helpers
 import sys
 
-text = sys.stdin.read()
-template = pyratemp.Template(string=text)
+template = pyratemp.Template(filename=sys.argv[1])
 print template(r=req.Req(), h=helpers.Helpers())
