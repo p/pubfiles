fbe5a53f2e9332334aad7667482d2a0e237aa9e7
Oleg Pudeyev <oleg@bsdpower.com>
2014-01-20 22:00:53 -0500

    Firefox is now back to firefox binary

--- home/openbox/lib/helpers.py
+++ home/openbox/lib/helpers.py
@@ -11,6 +11,7 @@ class Helpers:
     @run_as_browser
     def default_firefox_bin(self):
         candidates = [
+            '/usr/local/lib/firefox/firefox-bin'
             '/usr/local/lib/firefox3/firefox-bin'
         ]
         return self._pick(candidates, os.path.exists)
