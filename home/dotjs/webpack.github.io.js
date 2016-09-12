3d91726d83e9d071b8e1ce9161e69f9cc6685acf
Oleg Pudeyev <oleg@bsdpower.com>
2016-09-12 13:46:56 -0400

    dotjs (firefox) directory, with webpack cube elimination script as the first one

---
+++ home/dotjs/webpack.github.io.js
@@ -0,0 +1,6 @@
+// spinning cubes
+$('.cube, .cube-inner').remove()
+// the entire logo bar which is now empty
+$('.logo').remove()
+// fork me on github logo which now overlays navigation
+$('img[alt="Fork me on GitHub"]').remove()
