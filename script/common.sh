b1facd01fde7f021d302f0005e1f3dc8eb2a69e6
Oleg Pudeyev <oleg@bsdpower.com>
2017-10-31 19:32:17 -0400

    Tweaking installer

--- script/common.sh
+++ script/common.sh
@@ -2,10 +2,26 @@ have() {
   which "$1" >/dev/null 2>&1
 }
 
+is_thinkpad() {
+  grep -q LENOVO /sys/devices/virtual/dmi/id/chassis_vendor 2>/dev/null
+}
+
 is_laptop() {
   if have laptop-detect; then
     laptop-detect
   else
-    grep LENOVO /sys/devices/virtual/dmi/id/chassis_vendor
+    is_thinkpad
   fi
+}
+
+is_headless() {
+  grep -q 'Intel.*Q9300' /proc/cpuinfo
+}
+
+install_mm() {
+  ! grep -q 'Intel.*7700K' /proc/cpuinfo
+}
+
+have_user() {
+  usermod "$1" >/dev/null 2>&1
 }
