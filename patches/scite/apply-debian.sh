77893ff983b40d6ed27d943a43b2c77cb3f2c4be
Oleg Pudeyev <oleg@bsdpower.com>
2017-12-05 20:11:09 -0500

    Revise scite building script

--- patches/scite/apply-debian.sh
+++ patches/scite/apply-debian.sh
@@ -9,7 +9,7 @@ if test "$1" = -u; then
   if test `id -u` != 0; then
     exec sudo "$0" "$@"
   fi
-  
+
   apt-get build-dep scite
   apt-get install fakeroot libgtk2.0-dev debhelper
 fi
@@ -24,15 +24,12 @@ cwd="`pwd`"
 tmpdir="`mktemp -d`"
 
 cd "$tmpdir"
-wget https://raw.githubusercontent.com/p/freebsd-ports-changes/master/overlay/editors/scite/files/patch-tab-width
-#wget http://bsdpower.com/rebuild-scite-for-gtk2-on-debian-jessie/scite-debian-gtk2-patch.diff
-cp "$src"/scite-debian-gtk2-patch.diff .
 
 apt-get source scite
 cd scite-*
 (cd scite &&
-patch -p1 <../../patch-tab-width)
-patch -p0 <../scite-debian-gtk2-patch.diff
+patch -p1 <"$src"/patch-tab-width-4.0.0.diff)
+patch -p0 <"$src"/patch-debian-gtk2.diff
 sed -i -e s/libgtk-3-dev/libgtk2.0-dev/ debian/control
 
 # http://askubuntu.com/questions/226495/how-to-solve-dpkg-source-source-problem-when-building-a-package
