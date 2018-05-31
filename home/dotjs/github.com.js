cdb40610cd343fe73c93f439480f67e1976f4825
Oleg Pudeyev <oleg@bsdpower.com>
2018-05-31 12:27:47 -0400

    Kill off more github popups

--- home/dotjs/github.com.js
+++ home/dotjs/github.com.js
@@ -2,13 +2,12 @@ var css = '\
 .pr-toolbar {\
     position: static !important;\
 }\
+.Popover.js-notice, .Popover-message { display: none !important }\
 '
 
 var style = document.createElement('style')
 style.appendChild(document.createTextNode(css))
 document.getElementsByTagName('head')[0].appendChild(style)
-
-$('.Popover.js-notice').remove();
 
 $(document).scroll(function() {
     $('.is-stuck').removeClass('is-stuck');
