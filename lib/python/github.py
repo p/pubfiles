9fa78ef7a1bfb82dc1efd59fc3e831b84728a4e2
Oleg Pudeyev <oleg@bsdpower.com>
2020-10-07 11:16:42 -0400

    python 3 compatibility for github scripts

--- lib/python/github.py
+++ lib/python/github.py
@@ -1,8 +1,12 @@
-import urllib2, json, sys, re, subprocess, os.path
+try:
+    from urllib.request import urlopen
+except ImportError:
+    from urllib2 import urlopen
+import json, sys, re, subprocess, os.path
 
-class BranchLookupError(StandardError): pass
-class UnexpectedBranchName(StandardError): pass
-class UnexpectedRemoteUrl(StandardError): pass
+class BranchLookupError(Exception): pass
+class UnexpectedBranchName(Exception): pass
+class UnexpectedRemoteUrl(Exception): pass
 
 def get_current_branch():
     output = subprocess.check_output(['git', 'branch'])
@@ -13,7 +17,7 @@ def get_current_branch():
             break
             
     if branch is None:
-        raise BranchLookupError, 'Cannot figure out current branch'
+        raise BranchLookupError('Cannot figure out current branch')
         
     return branch
 
@@ -21,14 +25,14 @@ def get_current_pr_num():
     branch = get_current_branch()
     match = re.match(r'pr-(\d+)$', branch)
     if match is None:
-        raise UnexpectedBranchName, 'Current branch is not of pr-123 format'
+        raise UnexpectedBranchName('Current branch is not of pr-123 format')
 
     pr_num = int(match.group(1))
     return pr_num
 
 def get_remote_names():
     output = subprocess.check_output(['git', 'remote', '-v'])
-    lines = output.strip().split("\n")
+    lines = output.decode().strip().split("\n")
     names = [line.split("\t")[0] for line in lines]
     return names
 
@@ -40,7 +44,7 @@ def get_upstream_remote_name():
     elif 'origin' in names:
         name = 'origin'
     else:
-        raise RemoteLookupError, 'Cannot determine the upstream remote'
+        raise RemoteLookupError('Cannot determine the upstream remote')
         
     return name
 
@@ -52,7 +56,7 @@ def get_remote_fetch_url(remote_name):
 
 def get_remote_url(remote_name, kind):
     output = subprocess.check_output(['git', 'remote', '-v'])
-    lines = output.strip().split("\n")
+    lines = output.decode().strip().split("\n")
     
     remote_url = None
     for line in lines:
@@ -63,7 +67,7 @@ def get_remote_url(remote_name, kind):
                 break
 
     if remote_url is None:
-        raise RemoteLookupError, 'Cannot find remote %s' % remote_name
+        raise RemoteLookupError('Cannot find remote %s' % remote_name)
         
     return remote_url
 
@@ -72,7 +76,7 @@ def split_github_url(remote_url):
     if match is None:
         match = re.match(r'https://github.com/([\w-]+)/([\w-]+?)(.git)?$', remote_url)
         if match is None:
-            raise UnexpectedRemoteUrl, "Weird url: %s" % remote_url
+            raise UnexpectedRemoteUrl("Weird url: %s" % remote_url)
     
     return match.group(1), match.group(2)
 
@@ -80,7 +84,7 @@ def get_pr_info(upstream_owner_name, upstream_repo_name, pr_num):
     api_url = 'https://api.github.com/repos/%s/%s/pulls/%s' % (upstream_owner_name, upstream_repo_name, pr_num)
     print(api_url)
 
-    c = urllib2.urlopen(api_url).read()
+    c = urlopen(api_url).read()
     payload = json.loads(c)
 
     pr_owner_name = payload['head']['user']['login']
