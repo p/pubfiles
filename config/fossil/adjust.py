0f39b9ae67de09b449d3df202395e94fbeeeb308
Oleg Pudeyev <oleg@bsdpower.com>
2020-11-27 20:09:16 -0500

    convert all scripts to python3 shebangs

--- config/fossil/adjust.py
+++ config/fossil/adjust.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys, sqlite3, time as _time, os.path
 
--- home/openbox/lib/pyratemp.py
+++ home/openbox/lib/pyratemp.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Small, simple and powerful template-engine for python.
