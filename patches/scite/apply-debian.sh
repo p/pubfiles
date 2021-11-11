1ca335d9330b6bec684790fd503666d85b0257d9
Oleg Pudeyev <code@olegp.name>
2021-11-11 17:56:09 -0500

    bump version number

--- patches/scite/apply-debian.sh
+++ patches/scite/apply-debian.sh
@@ -32,6 +32,10 @@ patch -p1 <"$src"/patch-tab-width-5.1.3.diff)
 patch -p0 <"$src"/patch-debian-gtk2-5.1.3.diff
 sed -i -e s/libgtk-3-dev/libgtk2.0-dev/ debian/control
 
+set -x
+version=`head -n 1 debian/changelog |egrep -o '\(.*?\)' |tr -d '()'`
+dch -v "$version"+o "Oleg's patches"
+
 # http://askubuntu.com/questions/226495/how-to-solve-dpkg-source-source-problem-when-building-a-package
 dpkg-buildpackage -b
 cp ../scite_*.deb "$cwd"
