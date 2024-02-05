eb09992f31f5910eb8ee3431448354eb092603e4
Oleg Pudeyev <code@olegp.name>
2024-02-05 12:27:21 -0500

    user account inquiry

--- lib/ruby/global_helpers.rb
+++ lib/ruby/global_helpers.rb
@@ -1,7 +1,16 @@
+autoload :Etc, 'etc'
+
 module GlobalHelpers
 
   def have?(bin)
     ENV.fetch('PATH').split(':').any? { |path| File.exist?(File.join(path, bin)) }
+  end
+
+  def have_user?(username)
+    Etc.getpwnam(username)
+    true
+  rescue ArgumentError
+    false
   end
 
   def laptop?
