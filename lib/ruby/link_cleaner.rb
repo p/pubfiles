e0d421cd83c5e9879ad50364b480a9f1d5082b39
Oleg Pudeyev <code@olegp.name>
2025-07-18 19:48:33 -0400

    combine cleaners

--- lib/ruby/link_cleaner.rb
+++ lib/ruby/link_cleaner.rb
@@ -5,6 +5,16 @@ autoload :URI, 'uri'
 autoload :Find, 'find'
 
 class LinkCleaner
+  def initialize(method: nil)
+    unless [nil, :regexp, :nokogiri].include?(method)
+      raise "Invalid method: #{method}"
+    end
+    if method == :nokogiri
+      require 'nokogiri'
+    end
+    @method = method
+  end
+
   def run_in_path(start)
     Find.find(start) do |path|
       if path.end_with?('.md')
@@ -14,6 +24,35 @@ class LinkCleaner
   end
 
   def clean(contents)
+    case method
+    when :nokogiri
+      clean_via_nokogiri(contents)
+    else
+      clean_via_regexp(contents)
+    end
+  end
+
+  def clean_file_in_place(path)
+    contents = case path
+    when IO
+      path.read
+    else
+      File.read(path)
+    end
+
+    cleaned_contents = clean(contents)
+
+    if cleaned_contents != contents
+      puts "Writing #{path}"
+      File.open(path, 'w') do |f|
+        f << cleaned_contents
+      end
+    end
+  end
+
+  private
+
+  def clean_via_regexp(contents)
     cleaned = contents.gsub(%r,(https://www.amazon.com/[^/]+/dp/\w+)/ref=\w+(?:/[-\d]+)?\?\S+,, '\1')
     cleaned.gsub!(%r,(https://www.amazon.com/gp/product/\w+)/ref=\w+(?:/[-\d]+)?\?\S+,, '\1')
     cleaned.gsub!(%r,(https://www.ebay.com/itm(?:/[^/]+)?/\d+)\?\S+,) do |m|
@@ -37,16 +76,40 @@ class LinkCleaner
     cleaned.gsub!(/ +$/, '')
   end
 
-  def clean_file_in_place(path)
-    contents = File.read(path)
-
-    cleaned_contents = clean(contents)
-
-    if cleaned_contents != contents
-      puts "Writing #{path}"
-      File.open(path, 'w') do |f|
-        f << cleaned_contents
+  def clean_via_nokogiri(contents)
+    doc = Nokogiri::HTML(contents)
+    doc.xpath('//a').each do |a|
+      href = a.attr('href')
+      if href
+        cleaned_href = clean_url(href)
+        if href != cleaned_href
+          a.set_attribute('href', cleaned_href)
+        end
       end
+    end
+    doc.to_s
+  end
+
+  def clean_url(url)
+    if url.start_with?('https://www.google.com/url')
+      url = uri_class.parse(url)
+      q = url.query_values.fetch('q')
+      q
+    elsif url.start_with?('https://l.facebook.com/l.php')
+      url = uri_class.parse(url)
+      q = url.query_values.fetch('u')
+      q
+    else
+      url
+    end
+  end
+
+  def uri_class
+    @uri_class ||= begin
+      require 'addressable'
+      Addressable::URI
+    rescue LoadError
+      URI
     end
   end
 end
